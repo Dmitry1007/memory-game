0a49d30d379c69ca5c32b8c4eeae7423060c270c
Dmitry Sky <dmitry.sky@icloud.com>
2023-02-11 20:20:50 -0700

    4 column 8 row grid of squares

--- src/App.js
+++ src/App.js
@@ -1,5 +1,42 @@
 function App() {
-  return <h1 className="text-3xl font-bold underline">App!</h1>;
+  return (
+    <div className="h-screen w-screen bg-yellow-300">
+      <div className="grid grid-cols-4 gap-2 p-2">
+        <div className="aspect-square min-h-0 min-w-0 bg-gray-500"></div>
+        <div className="aspect-square min-h-0 min-w-0 bg-gray-500"></div>
+        <div className="aspect-square min-h-0 min-w-0 bg-gray-500"></div>
+        <div className="aspect-square min-h-0 min-w-0 bg-gray-500"></div>
+        <div className="aspect-square min-h-0 min-w-0 bg-gray-500"></div>
+        <div className="aspect-square min-h-0 min-w-0 bg-gray-500"></div>
+        <div className="aspect-square min-h-0 min-w-0 bg-gray-500"></div>
+        <div className="aspect-square min-h-0 min-w-0 bg-gray-500"></div>
+        <div className="aspect-square min-h-0 min-w-0 bg-gray-500"></div>
+        <div className="aspect-square min-h-0 min-w-0 bg-gray-500"></div>
+        <div className="aspect-square min-h-0 min-w-0 bg-gray-500"></div>
+        <div className="aspect-square min-h-0 min-w-0 bg-gray-500"></div>
+        <div className="aspect-square min-h-0 min-w-0 bg-gray-500"></div>
+        <div className="aspect-square min-h-0 min-w-0 bg-gray-500"></div>
+        <div className="aspect-square min-h-0 min-w-0 bg-gray-500"></div>
+        <div className="aspect-square min-h-0 min-w-0 bg-gray-500"></div>
+        <div className="aspect-square min-h-0 min-w-0 bg-gray-500"></div>
+        <div className="aspect-square min-h-0 min-w-0 bg-gray-500"></div>
+        <div className="aspect-square min-h-0 min-w-0 bg-gray-500"></div>
+        <div className="aspect-square min-h-0 min-w-0 bg-gray-500"></div>
+        <div className="aspect-square min-h-0 min-w-0 bg-gray-500"></div>
+        <div className="aspect-square min-h-0 min-w-0 bg-gray-500"></div>
+        <div className="aspect-square min-h-0 min-w-0 bg-gray-500"></div>
+        <div className="aspect-square min-h-0 min-w-0 bg-gray-500"></div>
+        <div className="aspect-square min-h-0 min-w-0 bg-gray-500"></div>
+        <div className="aspect-square min-h-0 min-w-0 bg-gray-500"></div>
+        <div className="aspect-square min-h-0 min-w-0 bg-gray-500"></div>
+        <div className="aspect-square min-h-0 min-w-0 bg-gray-500"></div>
+        <div className="aspect-square min-h-0 min-w-0 bg-gray-500"></div>
+        <div className="aspect-square min-h-0 min-w-0 bg-gray-500"></div>
+        <div className="aspect-square min-h-0 min-w-0 bg-gray-500"></div>
+        <div className="aspect-square min-h-0 min-w-0 bg-gray-500"></div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
